3545c73ce020b98a9520f2b6763043522f4204fc
Yogi2084 <1si22is406@sit.ac.in>
2025-03-27 12:08:31 +0530

    add user controller and routes for fetching user details and all users

--- src/routes/route-index.ts
+++ src/routes/route-index.ts
@@ -1,12 +1,16 @@
 import { Hono } from "hono";
 import { authenticationRoutes } from "./authentication-routes";
 import { logger } from "hono/logger";
+import { usersRoutes } from "./user-routes";
 
 export const allRoutes = new Hono();
 
 allRoutes.use(logger())
 
 allRoutes.route("/authentication", authenticationRoutes);
+
+allRoutes.route("/users", usersRoutes)
+
 
 allRoutes.get(
   "/health",
