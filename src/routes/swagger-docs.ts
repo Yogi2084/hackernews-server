ad9deb5f3f8255389c09f43106b6a81aaf819446
Yogi2084 <1si22is406@sit.ac.in>
2025-04-05 11:21:46 +0530

    update Swagger documentation with new version

--- src/routes/swagger-docs.ts
+++ src/routes/swagger-docs.ts
@@ -5,31 +5,31 @@ export const swaggerDocument = new Hono();
 swaggerDocument.get("/docs", (c) => {
   return c.json({
     openapi: "3.0.0",
-  info: {
-    title: "HackerNews API",
-    version: "2.0.3",
-    description: "HackerNews clone server",
-    contact: {
-      name: "HackerNews-Server",
-      url: "https://github.com/Mahesh5726/hackernews-server",
+    info: {
+      title: "Hackernews API",
+      version: "2.3.3",
+      description: "HackerNews clone server",
+      contact: {
+        name: "HackerNews-Server",
+        url: "https://github.com/Yogi2084/hackernews-server",
+      },
     },
-  },
-  servers: [
-    {
-      url: "http://localhost:3000",
-      description: "Development server",
-    },
-    {
-      url: "https://hackernews.lemonisland-20d31e0a.centralindia.azurecontainerapps.io/",
-      description: "Production server",
-    },
-  ],
-  tags: [
-    { name: "Authentication", description: "Authentication endpoints" },
-    { name: "Users", description: "User management endpoints" },
-    { name: "Posts", description: "Post management endpoints" },
-    { name: "Likes", description: "Like management endpoints" },
-    { name: "Comments", description: "Comment management endpoints" },
-  ],
+    servers: [
+      {
+        url: "http://localhost:3000",
+        description: "Development server",
+      },
+      {
+        url: "https://hackernews.ashysmoke-e7439f75.centralindia.azurecontainerapps.io",
+        description: "Production server",
+      },
+    ],
+    tags: [
+      { name: "Authentication", description: "Authentication endpoints" },
+      { name: "Users", description: "User management endpoints" },
+      { name: "Posts", description: "Post management endpoints" },
+      { name: "Likes", description: "Like management endpoints" },
+      { name: "Comments", description: "Comment management endpoints" },
+    ],
   });
 });
