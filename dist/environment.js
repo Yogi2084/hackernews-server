44c8c7fc02aef70744cdff0f9e31fa2d1b68bd70
Yogi2084 <1si22is406@sit.ac.in>
2025-05-10 12:47:51 +0530

    Remove likes and posts routes, controllers, and types; delete session middleware and user controllers; clean up routes index.

--- dist/environment.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.betterAuthSecret = exports.webClientUrl = exports.serverUrl = exports.jwtSecret = void 0;
-exports.jwtSecret = process.env.KEY || process.exit(1);
-exports.serverUrl = process.env.SERVER_URL || process.exit(1);
-exports.webClientUrl = process.env.WEB_CLIENT_URL || process.exit(1);
-exports.betterAuthSecret = process.env.BETTER_AUTH_SECRET || process.exit(1);
--- dist/src/lib/prisma/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prismaClient = void 0;
-const prisma_1 = require("../../generated/prisma");
-exports.prismaClient = new prisma_1.PrismaClient();
